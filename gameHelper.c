1771e06d82a1519db615a670efb26fd480eeb866
Matthew Marsicano <105601857+Matthew-Marsicano@users.noreply.github.com>
2024-07-30 00:05:57 -0400

    Update gameHelper.c

--- gameHelper.c
+++ gameHelper.c
@@ -1,2 +1,26 @@
 #include<gameHelper.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
+const char * genMove(){
+  srand(time(NULL));
+  int r = rand() % 3;
+  switch(r):
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2: 
+      return "Scissors;
+    default:
+      return "ERROR;
+}
+
+void victory(){
+  printf("Congrats you won!!");
+}
+void loss(){
+  printf("You lost, better luck next time!");
+}
